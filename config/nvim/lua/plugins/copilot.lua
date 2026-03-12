9875bbab5cfb7dc156bba656bc45552cbdaf2628
カツト <vincent.tantei@proton.me>
2026-03-12 19:56:17 +0100

    update

---
+++ config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        panel = { enabled = false },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-l>",
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+      })
+    end,
+  },
+}
