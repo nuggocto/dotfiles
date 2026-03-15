e3333068f0ea400a302bafb3506dfc784d77832c
カツト <vincent.tantei@proton.me>
2026-03-15 04:12:13 +0100

    setting up diffview

--- config/nvim/lua/plugins/git-diff.lua
+++ config/nvim/lua/plugins/git-diff.lua
@@ -17,4 +17,38 @@ return {
       },
     },
   },
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
+    keys = {
+      {
+        "<leader>gD",
+        "<cmd>DiffviewOpen<cr>",
+        desc = "Git diff review",
+      },
+      {
+        "<leader>gH",
+        "<cmd>DiffviewFileHistory %<cr>",
+        desc = "Git file history",
+      },
+      {
+        "<leader>gC",
+        "<cmd>DiffviewClose<cr>",
+        desc = "Git close review",
+      },
+    },
+    opts = {
+      view = {
+        default = {
+          disable_diagnostics = true,
+        },
+        file_history = {
+          disable_diagnostics = true,
+        },
+        merge_tool = {
+          disable_diagnostics = true,
+        },
+      },
+    },
+  },
 }
