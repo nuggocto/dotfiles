b62bf021cc2ae1d99bfc8c64291c4721e994acd3
カツト <vincent.tantei@proton.me>
2026-03-16 20:58:01 +0100

    custom indent

---
+++ config/nvim/lua/plugins/indent.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      indent = {
+        indent = {
+          char = "┊",
+        },
+        scope = {
+          char = "┊",
+        },
+      },
+    },
+  },
+}
