91ae5575dfecdf35b591bdd82bc09ef3b50cad9b
カツト <vincent.tantei@proton.me>
2026-03-17 18:44:27 +0100

    update and removing useless stuff

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -29,6 +29,14 @@ return {
 		},
 	},
 	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+		opts = {
+			variant = "dawn",
+		},
+	},
+	{
 		"LazyVim/LazyVim",
 		opts = {
 			colorscheme = "aether",
